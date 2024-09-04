d2a1909e9b133af90f66c39d332a2f3939b5e3e2
sunnydineva <114307463+sunnydineva@users.noreply.github.com>
2024-09-04 22:55:14 +0300

    User entity

---
+++ src/main/java/com/numerology/be/entity/User.java
@@ -0,0 +1,29 @@
+package com.numerology.be.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User() {
+    }
+}
