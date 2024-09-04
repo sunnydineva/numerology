09e8b6812dbddedded32fe54cf96281bd220e60a
sunnydineva <114307463+sunnydineva@users.noreply.github.com>
2024-09-04 22:55:14 +0300

    User entity

---
+++ src/main/java/com/numerology/be/NumerologyApplication.java
@@ -0,0 +1,13 @@
+package com.numerology.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumerologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumerologyApplication.class, args);
+	}
+
+}
